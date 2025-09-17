3d670aeebdb138b28c33ae5834c37a6652d6fdd5
Ayush <bhandariayush935@gmail.com>
2025-09-17 23:23:43 +0530

    Main App JSX File

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { useState } from 'react'
-import './App.css'
-import CodeHoverCards from './components/lightswindui/lightswind/code-hover-cards';
-// import { Github, Code, Dices } from 'lucide-react';
+import './App.css';
+import { useState } from 'react';
+import Signup from './pages/signup';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom'
+import { AuthProvider } from './context/authContext';
+import Home from './pages/home';
+import Login from './pages/login';
+import Dashboard from './pages/dashboard';
+import AdminDashboard from './pages/AdminDashboard';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    
-     <h1 className='text-amber-300 text-5xl' >This is the Page</h1>
-      <CodeHoverCards/>
-      
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/admin/dashboard' element={<AdminDashboard />} />
+          <Route path="/" element={<Home />} />
+
+          {/* Catch-all 404 */}
+          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+        </Routes>
+      </AuthProvider>
+
     </>
   )
 }
