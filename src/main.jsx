08143d0c4ed8afcc978f32befb0733070bc06899
Ayush <bhandariayush935@gmail.com>
2025-09-18 23:37:36 +0530

    Main Rendering File

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { RoleProvider } from './context/roleContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <BrowserRouter>
-    <App />
-     </BrowserRouter>
+    <BrowserRouter>
+      <RoleProvider>
+        <App />
+      </RoleProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
