0eea2d4150a1e04d46267fcdb2ca406950885a0f
Ayush <bhandariayush935@gmail.com>
2025-09-18 23:36:39 +0530

    Container File

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Signup from './pages/signup';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navigate } from 'react-router-dom'
@@ -8,24 +8,86 @@ import Home from './pages/home';
 import Login from './pages/login';
 import Dashboard from './pages/dashboard';
 import AdminDashboard from './pages/AdminDashboard';
+import Role from './pages/role'
+import ProtectedRoute from './routes/protectroute';
+import { RoleProvider } from './context/roleContext';
+import { supabase } from './api/supabaseClient';
 
 
 function App() {
+
+  const [role, setRole] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchRole = async () => {
+      const { data: { user } } = await supabase.auth.getUser()
+      if (!user) {
+        setLoading(false)
+        return;
+      }
+
+      const { data, error } = await supabase
+        .from('profiles')
+        .select('*')
+        .eq('id', user.id);
+
+      if (error) {
+        console.error(error.message);
+      } else {
+        console.log("Profiles data:", data);
+        if (data && data.length > 0) {
+          setRole(data[0].role);
+        }
+      }
+      setLoading(false)
+    }
+    fetchRole()
+  }, [])
+
+  useEffect(() => {
+    if (role) {
+      console.log("User Role (updated):", role);
+    }
+  }, [role]);
+
 
 
   return (
     <>
       <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/admin/dashboard' element={<AdminDashboard />} />
-          <Route path="/" element={<Home />} />
+        <RoleProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
 
-          {/* Catch-all 404 */}
-          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-        </Routes>
+
+            <Route path='/dashboard' element={
+              <ProtectedRoute >
+                <Dashboard />
+              </ProtectedRoute>} />
+
+
+            <Route path='/admin/dashboard' element={
+              <ProtectedRoute >
+                <AdminDashboard />
+              </ProtectedRoute>
+            } />
+
+            <Route path='/role' element={
+              <ProtectedRoute>
+                <Role />
+              </ProtectedRoute>
+            } />
+
+            <Route path="/" element={<ProtectedRoute>
+              <Home />
+            </ProtectedRoute>} />
+
+            {/* Catch-all 404 */}
+            <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+          </Routes>
+        </RoleProvider>
       </AuthProvider>
 
     </>
