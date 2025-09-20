299c58b940f1c42b6361e43ddc4cba7477175074
Ayush <bhandariayush935@gmail.com>
2025-09-20 12:06:56 +0530

    Backend File

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -2,11 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("Supabase URL:", process.env.SUPABASE_URL); // Debugging line
-console.log("Supabase Anon Key:", process.env.SUPABASE_ANON_KEY); // Debugging line
-
-const SUPABASE_URL = "https://nphkijivdcjztichcjps.supabase.co"
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5waGtpaml2ZGNqenRpY2hjanBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4Njk2NjgsImV4cCI6MjA3MzQ0NTY2OH0.TYaucKO_iyj2_a4sXo2BiYl4UBfAKbvehApqPOLuMJg"
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
   SUPABASE_URL,
